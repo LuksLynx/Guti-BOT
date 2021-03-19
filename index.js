e518ff6198c49f19da439d03f214b039afae93b7
Izurii <heitor_massarente@msn.com>
2021-03-19 18:40:05 -0300

    F95 command.

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ client.on('message', message => {          // onde os comandos ficam
     client.commands.get('play').execute(message, args);
   } else if (command === 'roll') {
     client.commands.get('roll').execute(message, args);
+  } else if (command === 'f95') {
+	client.commands.get('f95').execute(message, args);
   } else {
     message.channel.send('DIGITA CERTO SEU GORILA')
   }

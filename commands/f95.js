ec81589c76e83996eda3b8311ee214be7e15356f
Izurii Hootoh <heitor_massarente@msn.com>
2021-03-20 00:25:46 -0300

    Added format for tags and game url message

--- commands/f95.js
+++ commands/f95.js
@@ -3,19 +3,28 @@ const { PythonShell } = require('python-shell');
 module.exports = {
     name: 'f95',
     description: 'this is a ping command',
-    execute(message, args,) {
+    async execute(message, args,) {
 
-		let options = {
-			args: [JSON.stringify(this.token), '']
+        let tags = '';
+        
+        const split = message.content.split(" "); split.shift();
+        const tagsSplit = split.join(" ").trim();
+        tags = tagsSplit.replace(' ', '+');
+
+        let options = {
+			args: [JSON.stringify(this.token), tags]
 		};
+
+        let processMessage = await message.channel.send("Processando...");
 
 		PythonShell.run('./scripts/f95.py', options, (err, results) => {
 
 			if (err) {
-				console.log(err); return message.reply("Ihhh deu merda.");
+				console.log(err); return processMessage.edit("Ihhh deu merda.");
 			}
 
 			let result = JSON.parse(results[0]);
+            processMessage.edit(result['url']);
 			
 			if(result['token'])
 				this.token = JSON.parse(result['token']);

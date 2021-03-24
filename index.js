6e7aac13d9396a85431aa0284fc58477a327fe2c
LuksLynx <clonexzxd@hotmail.com>
2021-03-24 12:16:13 -0300

    passiva do guti now has a 10% chance of activating

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ for (const file of commandFiles) {
 }
 
 client.on('ready', () => {
-  client.user.setActivity('com o silvinha | %help');
+  client.user.setActivity('no porão de testes...');
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
@@ -23,11 +23,15 @@ client.on('message', async (message) => {
 
   /* Passiva do Guti */
 
-  let mensagens = ['Eu sou Guti', 'Guti?', 'Gay', 'Anthonny?'];
+  let mensagens = ['EU SOU O VERDADEIRO GUTI PORRA','VAI GANHA TATUAGEM DE SEREIA','MÃO NA CABEÇA','SAI DAQUI IMPOSTOR DE BOSTA','HEITOR FAZ ALGUMA COISA TIRA ESSE IMPOSTOR DO SERVIDOR','HEITOR PORRA, FAZ ALGUMA COISA... MOSTRA QUEM MANDA NESSA PORRA'];
   let random = await utils.random(0, mensagens.length);
+  let chamaGuti = await utils.random(1, 100);
+  
 
   if (message.content.includes('<@!231632637045768192>')) {
-    message.channel.send(mensagens[random]);
+    if (chamaGuti <= 10) {
+      message.channel.send(mensagens[random]);
+    }
   }
 
   /* Fim da passiva */
@@ -37,8 +41,8 @@ client.on('message', async (message) => {
   const args = message.content.slice(prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
 
-  if(client.commands.get(command) != undefined) {
-      client.commands.get(command).execute(message, args);
+  if (client.commands.get(command) != undefined) {
+    client.commands.get(command).execute(message, args);
   } else {
     message.channel.send('DIGITA CERTO SEU GORILA');
   }

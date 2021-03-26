1412f573b3080be699c98a5722022a209b41aea3
Izurii <heitor_massarente@msn.com>
2021-03-26 16:33:00 -0300

    Update help command

--- commands/help.js
+++ commands/help.js
@@ -5,84 +5,80 @@ module.exports = {
     description: 'help do bot',
     async execute(message, args) {
 
+		let botPrefix = process.env.BOT_PREFIX;
+		
+		let commands = {
+			audioCommands : ['play', 'stop', 'boss'],
+			utilityCommands : ['roll', 'clear'],
+			nsfwCommands : ['f95']
+		};
+
+		let commandsDescription = {
+			play : {
+				description : 'Toca um video do youtube',
+				usage : 'play` <vídeo>',
+				params: '`vídeo` aqui deve ser utilizado o link do video, caso não seja um link ele irá pesquisar 5 opções de nomes parecidos no youtube para você escolher.'
+			},
+			stop : {
+				description : 'Encerra qualquer comando de áudio que esteja em execução.',
+				usage : 'stop`',
+			},
+			boss : {
+				description : 'Toca o tema de um boss disponível a sua escolha.',
+				usage : 'boss`\n<número do boss>',
+				params: '`número do boss` deve ser digitado logo após a mensagem que contem os números dos bosses.'
+			},
+			roll : {
+				description : 'Rola um dado de 2 até 1000 com um modificador que pode ir até 1000.',
+				usage : 'roll` <dado><+ ou -><modificador>',
+				params: '`dado` escolha um dado de 2 até 1000.\n`+ ou -` escolha se quer somar ou subtrair o modificador.\n`modificador` escolha um modificador de 1 a 1000.'
+			},
+			clear : {
+				description : 'Deleta de 1 a 20 das mensagens mais recentes, tem um tempo de espera de 10 segundos para usar novamente.',
+				usage : 'clear` <número>',
+				params: '`número` é o número de mensagens que deseja deletar, sendo o máximo 20.'
+			},
+			f95 : {
+				description : 'Sorteia um jogo do fórum F95zone utilizando ou não tags',
+				usage : 'f95` <tag1> <tag2>...<tagN>',
+				params: '`tag` podem ser utilizadas inúmeras tags para sortear um jogo que contenha elas, caso não seja passada tag alguma, será sorteado um jogo da aba de updates mais recentes.'
+			},
+		};
+
+		var newEmbed;
+
         if(args == ''){
-        const newEmbed = new Discord.MessageEmbed()
+			
+			newEmbed = new Discord.MessageEmbed()
+				.setColor('#9400D3')
+				.setTitle('COMANDOS DO GUTÃO')
+				.addFields(
+					{ name: ':headphones: COMANDOS DE AUDIO', value: commands.audioCommands.map(item =>  `\`${botPrefix}${item}\``).join(" ") },
+					{ name: ':crossed_swords: COMANDOS DE UTILIDADE', value: commands.utilityCommands.map(item =>  `\`${botPrefix}${item}\``).join(" ") },
+					{ name: ':underage: COMANDOS NSFW', value: commands.nsfwCommands.map(item =>  `\`${botPrefix}${item}\``).join(" ") },
+				)
+				.setThumbnail('https://cdn.discordapp.com/attachments/426590652244164609/817549431225581598/tumblr_oof4yaMpvb1vy2tgqo1_400.png')
+				.setFooter(`Digite "${botPrefix}help <comando>" para mais detalhes`);
+
+        } else {
+
+			let selectedCommand = commandsDescription[args];
+
+			newEmbed = new Discord.MessageEmbed()
             .setColor('#9400D3')
-            .setTitle('COMANDOS DO GUTÃO')
+            .setTitle(`COMANDO ${botPrefix}${args}`)
             .addFields(
-                { name: ':headphones: COMANDOS DE AUDIO', value: '`%play`  `%stop`  `%boss`' },
-                { name: ':crossed_swords: COMANDOS DE UTILIDADE', value: '`%roll`  `%clear`' },
-                { name: ':underage: COMANDOS NSFW', value: '`%f95`' },
+                { name: ':mag:DESCRIÇÃO:', value: selectedCommand.description },
+                { name: ':question:COMO USAR:', value: `\`${botPrefix}` + selectedCommand.usage },
             )
-            .setThumbnail('https://cdn.discordapp.com/attachments/426590652244164609/817549431225581598/tumblr_oof4yaMpvb1vy2tgqo1_400.png')
-            .setFooter('Digite "%help <comando>" para mais detalhes')
-            message.channel.send(newEmbed);
-        }else if (args == 'play'){
-            const newEmbed = new Discord.MessageEmbed()
-            .setColor('#9400D3')
-            .setTitle('COMANDO %play')
-            .addFields(
-                { name: ':mag:DESCRIÇÃO:', value: 'Toca um video do youtube' },
-                { name: ':question:COMO USAR:', value: '`%play` <vídeo>' },
-                { name: ':gear:PARAMETRO(s) DO COMANDO:', value: '`vídeo` aqui deve ser utilizado o link do video, caso não seja um link ele irá pesquisar 5 opções de nomes parecidos no youtube para você escolher.' },
-            )
-            .setThumbnail('https://cdn.discordapp.com/attachments/426590652244164609/817549431225581598/tumblr_oof4yaMpvb1vy2tgqo1_400.png')
-            message.channel.send(newEmbed);
-        }else if (args == 'stop'){
-            const newEmbed = new Discord.MessageEmbed()
-            .setColor('#9400D3')
-            .setTitle('COMANDO %stop')
-            .addFields(
-                { name: ':mag:DESCRIÇÃO:', value: 'Encerra qualquer comando de áudio que esteja em execução.' },
-                { name: ':question:COMO USAR:', value: '`%stop`' },
-            )
-            .setThumbnail('https://cdn.discordapp.com/attachments/426590652244164609/817549431225581598/tumblr_oof4yaMpvb1vy2tgqo1_400.png')
-            message.channel.send(newEmbed);
-        }else if (args == 'boss'){
-            const newEmbed = new Discord.MessageEmbed()
-            .setColor('#9400D3')
-            .setTitle('COMANDO %boss')
-            .addFields(
-                { name: ':mag:DESCRIÇÃO:', value: 'Toca o tema de um boss disponível a sua escolha.' },
-                { name: ':question:COMO USAR:', value: '`%boss`\n<número do boss>' },
-                { name: ':gear:PARAMETRO(s) DO COMANDO:', value: '`número do boss` deve ser digitado logo após a mensagem que contem os números dos bosses.' },
-            )
-            .setThumbnail('https://cdn.discordapp.com/attachments/426590652244164609/817549431225581598/tumblr_oof4yaMpvb1vy2tgqo1_400.png')
-            message.channel.send(newEmbed);
-        }else if (args == 'roll'){
-            const newEmbed = new Discord.MessageEmbed()
-            .setColor('#9400D3')
-            .setTitle('COMANDO %roll')
-            .addFields(
-                { name: ':mag:DESCRIÇÃO:', value: 'Rola um dado de 2 até 1000 com um modificador que pode ir até 1000.' },
-                { name: ':question:COMO USAR:', value: '`%roll` <dado><+ ou -><modificador>' },
-                { name: ':gear:PARAMETRO(s) DO COMANDO:', value: '`dado` escolha um dado de 2 até 1000.\n`+ ou -` escolha se quer somar ou subtrair o modificador.\n`modificador` escolha um modificador de 1 a 1000.' },
-            )
-            .setThumbnail('https://cdn.discordapp.com/attachments/426590652244164609/817549431225581598/tumblr_oof4yaMpvb1vy2tgqo1_400.png')
-            message.channel.send(newEmbed);
-        }else if (args == 'clear'){
-            const newEmbed = new Discord.MessageEmbed()
-            .setColor('#9400D3')
-            .setTitle('COMANDO %clear')
-            .addFields(
-                { name: ':mag:DESCRIÇÃO:', value: 'Deleta de 1 a 20 das mensagens mais recentes, tem um tempo de espera de 10 segundos para usar novamente.' },
-                { name: ':question:COMO USAR:', value: '`%clear` <número>' },
-                { name: ':gear:PARAMETRO(s) DO COMANDO:', value: '`número` é o número de mensagens que deseja deletar, sendo o máximo 20.' },
-            )
-            .setThumbnail('https://cdn.discordapp.com/attachments/426590652244164609/817549431225581598/tumblr_oof4yaMpvb1vy2tgqo1_400.png')
-            message.channel.send(newEmbed);
-        }else if (args == 'f95'){
-            const newEmbed = new Discord.MessageEmbed()
-            .setColor('#9400D3')
-            .setTitle('COMANDO %f95')
-            .addFields(
-                { name: ':mag:DESCRIÇÃO:', value: 'Sorteia um jogo do fórum F95zone utilizando ou não tags' },
-                { name: ':question:COMO USAR:', value: '`%f95` <tag1> <tag2>...<tagN>' },
-                { name: ':gear:PARAMETRO(s) DO COMANDO:', value: '`tag` podem ser utilizadas inúmeras tags para sortear um jogo que contenha elas, caso não seja passada tag alguma, será sorteado um jogo da aba de updates mais recentes.' },
-            )
-            .setThumbnail('https://cdn.discordapp.com/attachments/426590652244164609/817549431225581598/tumblr_oof4yaMpvb1vy2tgqo1_400.png')
-            message.channel.send(newEmbed);
-        }
-        
+            .setThumbnail('https://cdn.discordapp.com/attachments/426590652244164609/817549431225581598/tumblr_oof4yaMpvb1vy2tgqo1_400.png');
+			
+			if(selectedCommand.params)
+				newEmbed.addField(':gear:PARAMETRO(s) DO COMANDO:', selectedCommand.params);
+
+		}
+
+		message.channel.send(newEmbed);
+
     }
 }

a602bb47a5b09eaff09c6a0d2fb7d6714b9ad104
LuksLynx <clonexzxd@hotmail.com>
2021-03-28 22:34:49 -0300

    new text format function

--- utils.js
+++ utils.js
@@ -1,3 +1,12 @@
 exports.random =  async (min, max) => {
 	return min + Math.floor((max - min) * Math.random());
 }
+
+exports.formatTextLimitCharacters = async (text, length=2043) => {
+	if((text == null) || (text.length <= length)) return text;
+
+	text = text.substring(0, length+3);
+	last = text.lastIndexOf(" ");
+	text = text.substring(0, last);
+	return text + "...";
+}

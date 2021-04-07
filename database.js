8c03aa4185d256a575e2e91bb40cb576f86a0f2d
Izurii <heitor_massarente@msn.com>
2021-04-07 15:31:43 -0300

    Getting mysql credentials from enviroment variables

--- database.js
+++ database.js
@@ -1,5 +1,10 @@
 const mysql = require ("mysql");
-const connection = mysql.createConnection({host:'localhost',user:'root',password:'Violeta60895',database:'GutiDB'});
+const connection = mysql.createConnection({
+	host:process.env.DB_HOST,
+	user:process.env.DB_USER,
+	password:process.env.DB_PASSWORD,
+	database:process.env.DB_NAME
+});
 
 connection.connect( error => {
     if(error) return console.error(error);

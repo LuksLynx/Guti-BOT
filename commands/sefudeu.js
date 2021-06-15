45783de4d4221f0f67c36939ef9850346bb84e7d
LuksLynx <79705957+LuksLynx@users.noreply.github.com>
2021-06-15 14:52:48 -0300

    Update sefudeu.js

--- commands/sefudeu.js
+++ commands/sefudeu.js
@@ -4,9 +4,9 @@ const Play = require("./play.js");
 module.exports = {
     name: 'sefudeu',
     description: 'o problema é seu',
-    async execute(message, args,) {
+    async execute(message, args) {
         
-        sefudeuUrl = 'https://www.youtube.com/watch?v=UrU-Ui9sh7M';
+        const sefudeuUrl = 'https://www.youtube.com/watch?v=UrU-Ui9sh7M';
 
         let voiceChannel = await Play.getVoiceChannel(message);
         if(!voiceChannel) return;

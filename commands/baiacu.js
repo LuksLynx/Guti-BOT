219083fb16d33a11c14ae2d505facf90157ec5cf
LuksLynx <79705957+LuksLynx@users.noreply.github.com>
2021-06-15 14:50:42 -0300

    Update baiacu.js

--- commands/baiacu.js
+++ commands/baiacu.js
@@ -1,16 +1,15 @@
-const ytdl = require('discord-ytdl-core');
 const Play = require("./play.js");
 
 module.exports = {
     name: 'baiacu',
     description: 'cenourinha hmmmm',
-    async execute(message, args,) {
+    async execute(message, args) {
         
-        sefudeuUrl = 'https://www.youtube.com/watch?v=290Ma1tbi-4';
+        const baiacuUrl = 'https://www.youtube.com/watch?v=290Ma1tbi-4';
 
         let voiceChannel = await Play.getVoiceChannel(message);
         if(!voiceChannel) return;
-        Play.playAudio(sefudeuUrl, voiceChannel);
+        Play.playAudio(baiacuUrl, voiceChannel);
 
     }
 }
